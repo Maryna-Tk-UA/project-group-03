4a10387ffce534b8c6671ab17d9e1c1c221bb743
Volodimir <sanin.vova200@gmail.com>
2025-08-09 13:24:52 +0300

    fix loader

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,9 +1,9 @@
 export function showLoader() {
   const loader = document.querySelector('.loader');
-  loader.classList.add('is-hidden');
+  loader.classList.remove('is-hidden');
 }
 
 export function hideLoader() {
   const loader = document.querySelector('.loader');
-  loader.classList.remove('is-hidden');
+  loader.classList.add('is-hidden');
 }
